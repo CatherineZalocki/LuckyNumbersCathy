c2927907517d5a57fbc1c8d191b7661a976f5ae0
CatherineZalocki <cathy.briglia@gmail.com>
2018-05-25 11:10:41 -0400

    first commit

---
+++ Lucky numbers (cathy)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky_numbers__cathy_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
